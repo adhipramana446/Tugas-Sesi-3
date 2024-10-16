93b07f9a0f268808d5d0cdba50b83471ebaa8d67
adhipramana446 <24106050051@student.uin-suka.ac.id>
2024-10-16 22:13:12 +0700

    Create perkalian.py

---
+++ perkalian.py
@@ -0,0 +1,8 @@
+def perkalian():
+    angka_pertama = int(input("Masukkan angka pertama: "))
+    angka_kedua = int(input("Masukkan angka kedua: "))
+    hasil = angka_pertama * angka_kedua
+    return hasil
+
+hasil = perkalian()
+print(f"Hasil perkalian: {hasil}")
